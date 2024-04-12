c5fcfa31b3c7b743b8f47dbcdcc63f84d9cb95bf
IgorPintoS <igorpintosantos05@gmail.com>
2024-04-11 21:03:59 -0300

    Quinto commit, método de listagem e gravação do evento. Tratamento de texto do slug.

--- src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
@@ -1,7 +1,12 @@
 package rocketseat.com.passin.repositories;
 
+import org.hibernate.graph.AttributeNode;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.attendee.Attendee;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    public List<Attendee> findByEventId(String eventId);
+
 }
